f8e21b7d22cd2b1acb2f0064bfa23a6936c67443
matloob9 <ahmed.mustafa86@gmail.com>
2021-03-14 15:55:34 +0200

    add New Test Integration Classes

---
+++ CustomerVehiclesCrudAPITest/CustomerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CustomerVehiclesCrudAPI.Models;
+using Xunit;
+using CustomerVehiclesCrudAPI;
+
+namespace CustomerVehiclesCrudAPITest
+{
+    public class CustomerTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public CustomerTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task TestGetCustomertemsAsync()
+        {
+            // Arrange
+            var request = "/api/Customer";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestGetCustomertemAsync()
+        {
+            // Arrange
+            var request = "/api/Customer/1";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostCustomertemAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/Customer",
+                Body = new
+                {
+                      customerName= "Ahmed Matloub",
+                      address="Egypt Cairo",
+                      
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPutCustomertemAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/Customer/4",
+                Body = new
+                {
+                    Id=4,
+                    customerName = "Ahmed Mostafa Matloub",
+                    address = "Egypt Cairo",
+
+
+                }
+            };
+
+            // Act
+            var response = await Client.PutAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+
+    }
+}
---
+++ CustomerVehiclesCrudAPITest/VehicleTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CustomerVehiclesCrudAPI.Models;
+using Xunit;
+using CustomerVehiclesCrudAPI;
+
+namespace CustomerVehiclesCrudAPITest
+{
+    public class VehicleTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public VehicleTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task TestGetVehicleItemsAsync()
+        {
+            // Arrange
+            var request = "/api/Vehicle";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestGetVehicleItemAsync()
+        {
+            // Arrange
+            var request = "/api/Vehicle/1";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPostVehicleItemAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/Vehicle",
+                Body = new
+                {
+                  vin= "Test13124",
+                  regNumber= "TestReg123",
+                  customerFk= 2,
+                  isConnected= true,
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestPutVehicleItemAsync()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/Vehicle/8",
+                Body = new
+                {
+                    Id = 8,
+                    vin = "Test1232124",
+                    regNumber = "TestReg123",
+                    customerFk = 2,
+                    isConnected = false,
+
+                }
+            };
+
+            // Act
+            var response = await Client.PutAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+       
+    }
+}
