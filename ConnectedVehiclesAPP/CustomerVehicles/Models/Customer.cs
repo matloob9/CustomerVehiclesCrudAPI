a4725843c925cc3b55c5e30ce4fd3e72670dde45
matloob9 <ahmed.mustafa86@gmail.com>
2021-03-16 23:22:23 +0200

    Add New App

---
+++ ConnectedVehiclesAPP/CustomerVehicles/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CustomerVehicles.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+    }
+}
